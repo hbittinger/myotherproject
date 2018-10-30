6c30f1f886d9eb29f0632f4bad616a61eb590e39
Holly <holly.bittinger@gmail.com>
2018-10-30 02:38:33 +0000

    initial commit

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    
+    
+    $(".logged-in-alert-close").click(function() {
+        $(this).parent().slideUp(250);
+    });
+})
---
+++ db/migrate/20181009014923_ass_user_id_to_posts.rb
@@ -0,0 +1,4 @@
+class AssUserIdToPosts < ActiveRecord::Migration
+  def change
+  end
+end
